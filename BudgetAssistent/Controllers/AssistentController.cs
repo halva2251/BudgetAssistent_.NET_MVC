319b821f031ef13eb0871b2df720e3ad710c6d24
Yen Sauliak <zenasaulak@gmail.com>
2024-11-29 13:30:09 +0100

    Implemented browse page

--- BudgetAssistent/Controllers/AssistentController.cs
+++ BudgetAssistent/Controllers/AssistentController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using BudgetAssistent.Data;
+using BudgetAssistent.Models;
 
 namespace BudgetAssistent.Controllers
 {
     public class AssistentController : Controller
     {
+        private ApplicationDbContext db;
+
+        public AssistentController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Expenses data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Expenses.Add(data);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Browse()
+        {
+            var data = db.Expenses.ToList();
+            return View(data);
         }
     }
 }

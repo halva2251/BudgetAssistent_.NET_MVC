095c7a9f7a7b58b01aaade6b161d37b88605e1cb
Yen Sauliak <zenasaulak@gmail.com>
2024-11-29 09:41:01 +0100

    Add project files.

---
+++ BudgetAssistent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BudgetAssistent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
